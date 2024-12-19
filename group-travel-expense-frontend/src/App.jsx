4b27da20f9e8f9833c970bb3bbd6899a80486a74
SeanFuXiao <fuxiaosean@gmail.com>
2024-12-18 23:58:41 -0500

    update

--- group-travel-expense-frontend/src/App.jsx
+++ group-travel-expense-frontend/src/App.jsx
@@ -1,58 +1,19 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Landing from "./pages/Landing";
 import Dashboard from "./pages/Dashboard";
 import TripDetails from "./pages/TripDetails";
-import AddTrip from "./pages/AddTrip";
-import AddBill from "./pages/AddBill";
-import SigninForm from "./pages/SigninForm";
-import SignupForm from "./pages/SignupForm";
-import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/signin" element={<SigninForm />} />
-          <Route path="/signup" element={<SignupForm />} />
-
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/trip/:id"
-            element={
-              <ProtectedRoute>
-                <TripDetails />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/add-trip"
-            element={
-              <ProtectedRoute>
-                <AddTrip />
-              </ProtectedRoute>
-            }
-          />
-          <Route
-            path="/add-bill"
-            element={
-              <ProtectedRoute>
-                <AddBill />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/trip/:id" element={<TripDetails />} />
+      </Routes>
     </Router>
   );
 }
